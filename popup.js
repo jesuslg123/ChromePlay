875d544ecc9184ead57ffae7b4d582c8e7fb356c
Jesus Lopez Garcia <jesuslg123@gmail.com>
2017-12-10 11:17:29 +0100

    Fixed popup

--- popup.js
+++ popup.js
@@ -1,11 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
 	
-	browser.tabs.getSelected(null, function(tab) {
-  		  var bgPage = browser.extension.getBackgroundPage();
-		  
-	      console.log("Starting AirPlaying...")
-  		  bgPage.startPlaying(tab.url)
-	   }
-	);
+
+	var querying = browser.tabs.query({currentWindow: true, active: true});
+	querying.then(currentTabStart, onError)
+
+	function currentTabStart(tabs) {
+		var url = undefined
+		for (let tab of tabs) {
+		  // tab.url requires the `tabs` permission
+		  url = tab.url
+		}
+
+		var bgPage = browser.extension.getBackgroundPage();
+		if (bgPage != undefined && url != undefined) {
+			console.log("Starting AirPlaying...")
+			bgPage.startPlaying(tab.url)
+		}
+	}
+
+	function onError(error) {
+		console.log(`Tab Error: ${error}`)
+	}
 		
 });
